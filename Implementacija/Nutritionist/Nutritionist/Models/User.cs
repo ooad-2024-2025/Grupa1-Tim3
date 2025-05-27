bd5733ed072b06fa4161e9509a6a3c7d1198c4ec
EnsarDzaf0 <ensardzafo03@gmail.com>
2025-05-27 10:17:56 +0200

    Display names and bosnian language

--- Implementacija/Nutritionist/Nutritionist/Models/User.cs
+++ Implementacija/Nutritionist/Nutritionist/Models/User.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Nutritionist.Models
 {
     public class User : IdentityUser<Guid>
     {
+        [Display(Name = "Korisničko ime")]           
+        public override string UserName
+        {
+            get => base.UserName;
+            set => base.UserName = value;
+        }
+
+        [Display(Name = "Telefon")]                    
+        public override string PhoneNumber
+        {
+            get => base.PhoneNumber;
+            set => base.PhoneNumber = value;
+        }
+        [Display(Name = "Ime")]
         public string FirstName { get; set; }
+        [Display(Name = "Prezime")]
         public string LastName { get; set; }
+        [Display(Name = "Spol")]
         public Gender Gender { get; set; }
+        [Display(Name = "Datum rodjenja")]
         public DateTime Birthday { get; set; }
+        [Display(Name = "Datum kreiranja")]
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [Display(Name = "Newsletter pretplatnik")]
         public bool IsSubscribedToNewsletter { get; set; } = false;
 
         public ICollection<Recipe> Recipes { get; set; }
